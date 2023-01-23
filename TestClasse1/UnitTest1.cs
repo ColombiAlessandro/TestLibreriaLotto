fb256f51b4028b6fa5ff4b7db51764459ebed5f5
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2023-01-23 09:30:14 +0100

    TestLibreria
    test completo

---
+++ TestClasse1/UnitTest1.cs
@@ -0,0 +1,83 @@
+using Lotto;
+namespace TestClasse1
+{
+    public class UnitTest1
+    {
+        Lotto.Lotto lotto;
+        [Fact]
+        public void Test1()
+        {
+            lotto = new Lotto.Lotto("012", "12/11/05");
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            int[] estrazioni = lotto.RegistroEstrazioni();
+            Assert.True(estrazioni[1] != 0);
+
+        }
+        [Fact]
+        public void Test2()
+        {
+            lotto = new Lotto.Lotto("012", "12/11/05");
+            lotto.EstraiSingolo();
+            int[] estrazioni = lotto.RegistroEstrazioni();
+            Assert.True(estrazioni[1] == 0);
+
+        }
+        [Fact]
+        public void Test3()
+        {
+            lotto = new Lotto.Lotto("012", "12/11/05");
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            Assert.Throws<Exception>(() => lotto.EstraiSingolo());
+
+        }
+        [Fact]
+        public void Test4()
+        {
+            lotto = new Lotto.Lotto("012", "12/11/05");
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.EstraiSingolo();
+            lotto.Reset();
+            Assert.True(lotto.RegistroEstrazioni()[0] == 0);
+        }
+        [Fact]
+        public void Test5()
+        {
+            int[] prova = new int[90];
+            lotto = new Lotto.Lotto("012", "12/11/05",prova,90,0);
+            
+            Assert.Throws<Exception>(() => lotto.EstraiSingolo());
+        }
+        [Fact]
+        public void Test6()
+        {
+            int[] i = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            lotto = new Lotto.Lotto("012", "12/11/05", i, 9, 0);
+            int[] j = new int[] { 1, 2, 3, 4, 5 };
+            Assert.True(lotto.VerificaEstrazioni(j,4)==4);
+        }
+        [Fact]
+        public void Test7()
+        {
+            int[] i = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            lotto = new Lotto.Lotto("012", "12/11/05", i, 9, 0);
+            int[] j = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Throws<Exception>(() => lotto.VerificaEstrazioni(j,7));
+        }
+        [Fact]
+        public void Test8()
+        {
+            int[] i = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            lotto = new Lotto.Lotto("012", "12/11/05", i, 9, 0);
+            int[] j = new int[] { 1, 2, 3, 4, 5, 6 };
+            Assert.Throws<Exception>(() => lotto.VerificaEstrazioni(j, 4));
+        }
+    }
+}
